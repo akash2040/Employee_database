4359f22142e960a0ad6f4b9dba066c39b4c4e07c
Akash Chanara <akashchanara@gmail.com>
2022-11-01 20:13:46 -0700

    create prompt

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const mysql = require("mysql2");
 const inquirer = require("inquirer");
 require("console.table");
 
-const connection = mysql.createConnection(
+const database = mysql.createConnection(
   // sql connect
   {
     host: "localhost",
@@ -12,3 +12,54 @@ const connection = mysql.createConnection(
   },
   console.log(`Connected to database employees_db.`)
 );
+function promptInfo() {
+  inquirer
+    .prompt({
+      type: "list",
+      name: "option",
+      message: "What would you like to do?",
+      choices: [
+        "View Departments",
+        "View Roles",
+        "View Employees",
+        "Add Department",
+        "Add Role",
+        "Add Employee",
+        "Update Employee Role",
+        "Update Employees Manager",
+        "Exit",
+      ],
+    })
+    .then(function ({ option }) {
+      switch (option) {
+        case "View  Departments":
+          viewAllDepartment();
+          break;
+        case "View Roles":
+          viewRoles();
+          break;
+        case "View Employees":
+          viewAllEmployees();
+          break;
+        case "Add Department":
+          addDepartment();
+          break;
+        case "Add Role":
+          addRole();
+          break;
+        case "Add Employee":
+          addEmployee();
+          break;
+        case "Update Employee Role":
+          updateEmployeeRole();
+          break;
+        case "Update Employees Manager":
+          updateManager();
+          break;
+        case "Exit":
+          database.end();
+          break;
+      }
+    });
+}
+promptInfo();
