53e451fccba4d02c2e4573f04ff5dd31eda75beb
Akash Chanara <akashchanara@gmail.com>
2022-11-01 20:22:35 -0700

    function add for employee

--- server.js
+++ server.js
@@ -143,3 +143,38 @@ function roleAdd() {
       );
     });
 }
+function employeeAdd() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "firstName",
+        message: "What is the employee's first name?",
+      },
+      {
+        type: "input",
+        name: "lastName",
+        message: "What is the employee's last name?",
+      },
+      {
+        type: "input",
+        name: "roleId",
+        message: "What is the employees role id?",
+      },
+      {
+        type: "input",
+        name: "managerId",
+        message: "What is the employees manager id if they have one?",
+      },
+    ])
+    .then(function ({ firstName, lastName, roleId, managerId }) {
+      connection.query(
+        `INSERT INTO employee(first_name, last_name, role_id, manager_id) VALUES ('${firstName}', '${lastName}','${roleId}',${managerId})`,
+        function (err, data) {
+          if (err) throw err;
+          console.log("Employee Added");
+          promptInfo();
+        }
+      );
+    });
+}
